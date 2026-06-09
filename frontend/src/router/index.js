18f322d6fa1fae30673858291f1806f2a07256b5
unknown <rodriguezedward6mbmq@gmail.com>
2026-06-09 16:48:04 +0800

    bugfix: true last

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { authState, isAuthed, loadMe } from '../stores/auth'
 
 const Portal = () => import('../views/Portal.vue')
@@ -42,7 +42,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
